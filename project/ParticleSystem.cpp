3ea0706a2a79b7bae27ba514e2f17687fb3758fc
ruben-source <51800810+ruben-source@users.noreply.github.com>
2019-12-12 11:13:12 +0100

    PS.cpp is good now

--- project/ParticleSystem.cpp
+++ project/ParticleSystem.cpp
@@ -1,1 +1,29 @@
 #include "ParticleSystem.h"
+
+/*
+	behöver ta in particles på något sätt, antar att den ska finns i main på något sätt
+*/
+
+
+// Methods
+void ParticleSystem::kill(int id) {
+	particles.erase(particles.begin() + id);
+}
+
+void ParticleSystem::spawn(Particle particle) {
+	particles.push_back(particle);
+}
+
+void ParticleSystem::process_particles(float dt) {
+	for (int i = 0; i < particles.size(); i++) {
+		Particle p = particles[i];
+		if (p.lifetime + dt > p.life_length)
+			kill(i);
+	}
+
+	for (int i = 0; i < particles.size(); i++) {
+		Particle p = particles[i];
+		p.lifetime = p.lifetime + dt;
+		p.pos = p.pos + p.velocity * dt;
+	}
+}
